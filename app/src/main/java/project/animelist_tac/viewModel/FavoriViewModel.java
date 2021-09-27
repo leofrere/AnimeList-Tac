229fde44f168b43a76d9419b3e9cd43227f59557
leofrere <leo.frere.etu@univ-lille.fr>
2021-09-27 10:17:17 +0200

    add FavoriViewModel

---
+++ app/src/main/java/project/animelist_tac/viewModel/FavoriViewModel.java
@@ -0,0 +1,13 @@
+package project.animelist_tac.viewModel;
+
+import android.view.View;
+
+import androidx.lifecycle.ViewModel;
+
+public class FavoriViewModel extends ViewModel {
+    private View view;
+
+    public FavoriViewModel(View view) {
+            this.view = view;
+    }
+}
