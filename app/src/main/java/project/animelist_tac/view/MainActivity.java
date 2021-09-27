195f46020585a08c2560a3d945879a97668181cd
leofrere <leo.frere.etu@univ-lille.fr>
2021-09-27 10:32:26 +0200

    set the navigation on main Activity

--- app/src/main/java/project/animelist_tac/view/MainActivity.java
+++ app/src/main/java/project/animelist_tac/view/MainActivity.java
@@ -1,8 +1,14 @@
 package project.animelist_tac.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import project.animelist_tac.R;
 
@@ -12,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView nav = findViewById(R.id.nav);
+        nav.setOnItemSelectedListener(new  NavigationBarView.OnItemSelectedListener(){
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                Fragment selectedFrag = null;
+
+                switch (item.getItemId()){
+                    case R.id.searchFragment:
+                        selectedFrag = new SearchFragment();
+                        break;
+                    case R.id.favoriteFragment:
+                        selectedFrag = new FavoriFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFrag).commit();
+                return true;
+            };
+
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
+
     }
 }
