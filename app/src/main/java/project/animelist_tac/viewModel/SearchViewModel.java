5d43ef2255f1732c5a939c09f7743f9594b4e008
leofrere <leo.frere.etu@univ-lille.fr>
2021-09-27 10:16:00 +0200

    add SearchViewModel

---
+++ app/src/main/java/project/animelist_tac/viewModel/SearchViewModel.java
@@ -0,0 +1,13 @@
+package project.animelist_tac.viewModel;
+
+import android.view.View;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchViewModel extends ViewModel {
+    private View view;
+
+    public SearchViewModel(View view) {
+        this.view = view;
+    }
+}
