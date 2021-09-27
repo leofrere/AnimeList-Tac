48983aa35a351faf1f300c3c60c34af46a569e4d
leofrere <leo.frere.etu@univ-lille.fr>
2021-09-27 10:03:37 +0200

    move MainActivity in view package

--- app/src/main/java/project/animelist_tac/view/MainActivity.java
+++ app/src/main/java/project/animelist_tac/view/MainActivity.java
@@ -1,8 +1,10 @@
-package project.animelist_tac;
+package project.animelist_tac.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import project.animelist_tac.R;
 
 public class MainActivity extends AppCompatActivity {
 
