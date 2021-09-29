03d7b8fb30550ad3d129b06819b7f59f8146a3af
arthas59 <jeremy.curoux.etu@univ-lille.fr>
2021-09-29 10:56:02 +0200

    add request in AnimeDataWeb add access internet in AndroidManifest.xml add implementation of okhttp

--- app/src/main/java/project/animelist_tac/Data/AnimeDataWeb.java
+++ app/src/main/java/project/animelist_tac/Data/AnimeDataWeb.java
@@ -1,5 +1,23 @@
 package project.animelist_tac.Data;
 
-public class AnimeDataWeb {
+import android.app.DownloadManager;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class AnimeDataWeb {
+    private int limit = 20;
+    // La majeure parti du code pour créer des requête provient de https://rapidapi.com/theapiguy/api/jikan1/
+    public com.squareup.okhttp.Request requestAllAnime(String searchString){
+        com.squareup.okhttp.Request request = new com.squareup.okhttp.Request.Builder()
+                .url("https://jikan1.p.rapidapi.com/search/anime?q=" + searchString + "&limit="+limit)
+                .get()
+                .addHeader("x-rapidapi-host", "jikan1.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "c09a52467emsh5bc86d47ca19a3bp14d8bcjsn71c384fcf21f")
+                .build();
+        return request;
+    }
 }
