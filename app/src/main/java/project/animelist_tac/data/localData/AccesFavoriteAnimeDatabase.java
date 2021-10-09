0550b4be5608a2f870538afa8b570a1de81dfe59
leofrere <leofrere94@gmail.com>
2021-10-09 10:28:22 +0200

    add in AccesFavoriteAnimeDatabase method to get one or all favorite anime

--- app/src/main/java/project/animelist_tac/data/localData/AccesFavoriteAnimeDatabase.java
+++ app/src/main/java/project/animelist_tac/data/localData/AccesFavoriteAnimeDatabase.java
@@ -1,7 +1,11 @@
 package project.animelist_tac.data.localData;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import project.animelist_tac.model.Anime;
 
@@ -27,6 +31,46 @@ public class AccesFavoriteAnimeDatabase {
         sqLiteDatabase = database.getWritableDatabase();
         String request = "delete from anime where mal_id=" + mal_id;
         sqLiteDatabase.execSQL(request);
+    }
+
+    public Anime getFavoriteAnime(int mal_id){
+        sqLiteDatabase = database.getReadableDatabase();
+        Anime anime = null;
+        String request = "select * from anime where mal_id=" + mal_id;
+        Cursor cursor = sqLiteDatabase.rawQuery(request, null);
+        cursor.moveToFirst();
+        if (cursor.isFirst()){
+            anime = createAnime(cursor);
+        }
+        return anime;
+    }
+
+    public List<Anime> getAllFavoriteAnime(){
+        LinkedList<Anime> favoriteAnimeList = new LinkedList<Anime>();
+        sqLiteDatabase = database.getReadableDatabase();
+        String request = "select * from anime";
+        Cursor cursor = sqLiteDatabase.rawQuery(request, null);
+        cursor.moveToFirst();
+        if (cursor.isFirst()){
+            while (!cursor.isAfterLast()){
+                favoriteAnimeList.add(createAnime(cursor));
+                cursor.moveToNext();
+            }
+        }
+        return favoriteAnimeList;
+    }
+
+    private Anime createAnime(Cursor cursor) {
+        Anime anime;
+        anime = new Anime(cursor.getInt(0));
+        anime.title(cursor.getString(1));
+        anime.synopsis(cursor.getString(2));
+        anime.dateDebut(cursor.getString(3));
+        anime.dateFin(cursor.getString(4));
+        anime.type(cursor.getString(5));
+        anime.imgUrl(cursor.getString(6));
+        anime.nbEpisode(cursor.getInt(7));
+        return anime;
     }
 
 }
