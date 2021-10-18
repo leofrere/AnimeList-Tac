dc4275072833f54e5459090785cf5452cc5979bd
arthas59 <jeremy.curoux.etu@univ-lille.fr>
2021-10-18 10:43:02 +0200

    add DataRepository class

---
+++ app/src/main/java/project/animelist_tac/data/DataRepository.java
@@ -0,0 +1,64 @@
+package project.animelist_tac.data;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.view.View;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import project.animelist_tac.data.webData.AnimeWebDatabase;
+import project.animelist_tac.model.Anime;
+import project.animelist_tac.viewModel.SearchViewModel;
+
+public class DataRepository {
+    private AnimeWebDatabase distanteRepository;
+
+
+    public DataRepository(Context context){
+        distanteRepository = new AnimeWebDatabase();
+    }
+
+    public void searchAnime(String search){
+        AsyncTaskRunner runner = new AsyncTaskRunner(search);
+        runner.execute();
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, String, List<Anime>> {
+
+        private String searchString;
+        private View view;
+        private SearchViewModel viewModel;
+        private List<Anime> animeList;
+
+        public AsyncTaskRunner(String search){
+            searchString = search;
+        }
+
+        public List<Anime> animeList(){
+            return animeList;
+        }
+
+        @Override
+        protected List<Anime> doInBackground(String... params) {
+            try {
+                return distanteRepository.getAllAnime(searchString);
+            }catch (Exception e){
+                return new LinkedList<Anime>() {
+                };
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Anime> result) {
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+        }
+    }
+}
