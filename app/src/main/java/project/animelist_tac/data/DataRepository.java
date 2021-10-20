9883dd45ef44f59b54da000c048a45edd069f661
leofrere <leofrere94@gmail.com>
2021-10-20 12:07:17 +0200

    add access to local repository in DataRepository

--- app/src/main/java/project/animelist_tac/data/DataRepository.java
+++ app/src/main/java/project/animelist_tac/data/DataRepository.java
@@ -7,18 +7,41 @@ import android.view.View;
 import java.util.LinkedList;
 import java.util.List;
 
+import project.animelist_tac.data.localData.AccesFavoriteAnimeDatabase;
 import project.animelist_tac.data.webData.AnimeWebDatabase;
 import project.animelist_tac.model.Anime;
 import project.animelist_tac.viewModel.SearchViewModel;
 
 public class DataRepository {
+
     private AnimeWebDatabase distanteRepository;
+    private AccesFavoriteAnimeDatabase favoriteAnimeDatabase;
 
 
     public DataRepository(Context context){
+        favoriteAnimeDatabase = new AccesFavoriteAnimeDatabase(context);
         distanteRepository = new AnimeWebDatabase();
     }
 
+    //Local
+    public void addFavoriteAnime(Anime anime){
+        favoriteAnimeDatabase.addFavoriteAnime(anime);
+    }
+
+    public void deleteFavoriteAnime(int mal_id){
+        favoriteAnimeDatabase.deleteFavoriteAnime(mal_id);
+    }
+
+    public Anime getFavoriteAnime(int mal_id){
+        return favoriteAnimeDatabase.getFavoriteAnime(mal_id);
+    }
+
+    public List<Anime> getAllFavoriteAnime(){
+        return favoriteAnimeDatabase.getAllFavoriteAnime();
+    }
+
+
+    //Distance
     public void searchAnime(String search){
         AsyncTaskRunner runner = new AsyncTaskRunner(search);
         runner.execute();
