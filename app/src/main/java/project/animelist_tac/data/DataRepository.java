08be84cb3a187df11280ef452077e1322c0bd63e
leofrere <leofrere94@gmail.com>
2021-10-23 15:31:12 +0200

    add isFavoriteAnime and set action in postExecute of asyncTask

--- app/src/main/java/project/animelist_tac/data/DataRepository.java
+++ app/src/main/java/project/animelist_tac/data/DataRepository.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.view.View;
 
+import androidx.lifecycle.ViewModel;
+
 import java.util.LinkedList;
 import java.util.List;
 
+import project.animelist_tac.adapter.SearchAdapter;
 import project.animelist_tac.data.localData.AccesFavoriteAnimeDatabase;
 import project.animelist_tac.data.webData.AnimeWebDatabase;
 import project.animelist_tac.model.Anime;
@@ -32,6 +35,10 @@ public class DataRepository {
         favoriteAnimeDatabase.deleteFavoriteAnime(mal_id);
     }
 
+    public boolean isFavoriteAnime(int mal_id){
+        return favoriteAnimeDatabase.isFavoriteAnime(mal_id);
+    }
+
     public Anime getFavoriteAnime(int mal_id){
         return favoriteAnimeDatabase.getFavoriteAnime(mal_id);
     }
@@ -42,18 +49,22 @@ public class DataRepository {
 
 
     //Distance
-    public void searchAnime(String search){
-        AsyncTaskRunner runner = new AsyncTaskRunner(search);
+    public void searchAnime(String search, SearchViewModel viewModel){
+        AsyncTaskRunner runner = new AsyncTaskRunner(search, viewModel, this);
         runner.execute();
     }
 
     private class AsyncTaskRunner extends AsyncTask<String, String, List<Anime>> {
 
         private String searchString;
+        private SearchViewModel viewModel;
         private List<Anime> animeList;
+        private DataRepository dataRepository;
 
-        public AsyncTaskRunner(String search){
+        public AsyncTaskRunner(String search, SearchViewModel viewModel, DataRepository dataRepository){
             searchString = search;
+            this.viewModel = viewModel;
+            this.dataRepository = dataRepository;
         }
 
         public List<Anime> animeList(){
@@ -72,6 +83,8 @@ public class DataRepository {
 
         @Override
         protected void onPostExecute(List<Anime> result) {
+            SearchAdapter adapter = new SearchAdapter(result, dataRepository);
+            viewModel.setAdapter(adapter);
         }
 
         @Override
