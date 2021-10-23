034f6a042b9390dec1d8c7fe05e7b58fcd0c343b
leofrere <leofrere94@gmail.com>
2021-10-23 15:32:06 +0200

    set recyclerView add action on search submit and add the view binding

--- app/src/main/java/project/animelist_tac/view/SearchFragment.java
+++ app/src/main/java/project/animelist_tac/view/SearchFragment.java
@@ -4,23 +4,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import project.animelist_tac.R;
+import project.animelist_tac.databinding.SearchFragmentBinding;
 import project.animelist_tac.viewModel.SearchViewModel;
 
 public class SearchFragment extends Fragment {
     private SearchViewModel viewModel;
+    private RecyclerView.LayoutManager layoutManager;
+    private SearchFragmentBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.search_fragment, container, false);
-        viewModel = new SearchViewModel(view);
+        binding = SearchFragmentBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        layoutManager = new LinearLayoutManager(view.getContext());
+        binding.searchRecycler.setLayoutManager(layoutManager);
+        viewModel = new SearchViewModel(this);
+        binding.searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                viewModel.searchAction(s);
+                binding.searchBar.clearFocus();
+                return true;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
         return view;
+    }
+
+    public RecyclerView searchRecylerView(){
+        return binding.searchRecycler;
     }
 }
