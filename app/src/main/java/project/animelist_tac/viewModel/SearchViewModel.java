0eb158e22c76f2748a5318c66c209deb500b672d
leofrere <leofrere94@gmail.com>
2021-10-23 15:32:34 +0200

    add method searchAction and setAdapter

--- app/src/main/java/project/animelist_tac/viewModel/SearchViewModel.java
+++ app/src/main/java/project/animelist_tac/viewModel/SearchViewModel.java
@@ -2,12 +2,32 @@ package project.animelist_tac.viewModel;
 
 import android.view.View;
 
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModel;
+import androidx.recyclerview.widget.RecyclerView;
+
+import project.animelist_tac.data.DataRepository;
+import project.animelist_tac.view.SearchFragment;
 
 public class SearchViewModel extends ViewModel {
-    private View view;
 
-    public SearchViewModel(View view) {
-        this.view = view;
+    private SearchFragment fragment;
+    private DataRepository dataRepository;
+
+    public SearchViewModel(SearchFragment fragment) {
+        this.fragment = fragment;
+        dataRepository = new DataRepository(fragment.getContext());
+        dataRepository.searchAnime("att", this);
     }
+
+    public void searchAction(String searchString){
+        if (searchString.length() > 3){
+            dataRepository.searchAnime(searchString, this);
+        }
+    }
+
+    public void setAdapter(RecyclerView.Adapter adapter){
+        fragment.searchRecylerView().setAdapter(adapter);
+    }
+
 }
