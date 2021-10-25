48685a3cd44440e44ababa83972b65190ddcf1b7
curoux <jeremy.curoux.etu@univ-lille.fr>
2021-10-25 16:07:53 +0200

    set recyclerview

--- app/src/main/java/project/animelist_tac/view/FavoriFragment.java
+++ app/src/main/java/project/animelist_tac/view/FavoriFragment.java
@@ -8,17 +8,30 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import project.animelist_tac.R;
+import project.animelist_tac.adapter.FavoriAdapter;
+import project.animelist_tac.data.DataRepository;
+import project.animelist_tac.databinding.FavoriFragmentBinding;
 import project.animelist_tac.viewModel.FavoriViewModel;
 
 public class FavoriFragment extends Fragment {
     private FavoriViewModel viewModel;
+    private RecyclerView.LayoutManager layoutManager;
+    private FavoriFragmentBinding binding;
+    private DataRepository dataRepository;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.favori_fragment, container, false);
+        binding = FavoriFragmentBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        dataRepository = new DataRepository(view.getContext());
+        layoutManager = new LinearLayoutManager(view.getContext());
+        binding.favoriteRecyclerView.setLayoutManager(layoutManager);
+        FavoriAdapter favoriAdapter = new FavoriAdapter(dataRepository.getAllFavoriteAnime());
+        binding.favoriteRecyclerView.setAdapter(favoriAdapter);
         viewModel = new FavoriViewModel(view);
 
         return view;
