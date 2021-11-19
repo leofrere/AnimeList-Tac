9afe58429cdd9cb4b9e79a9b24f5a449284a06a3
leofrere <leofrere94@gmail.com>
2021-11-19 14:34:01 +0100

    add JinkanClient

---
+++ app/src/main/java/project/animelist_tac/data/webData/JinkanClient.java
@@ -0,0 +1,39 @@
+package project.animelist_tac.data.webData;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class JinkanClient {
+
+    public static String BASE_URL = "https://jikan1.p.rapidapi.com/search/anime/";
+
+    private static Retrofit retrofit;
+
+    private static JinkanClient instance = null;
+
+    private JinkanInterface myAPI;
+
+    public JinkanClient (){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                    .build();
+        }
+        myAPI = retrofit.create(JinkanInterface.class);
+    }
+
+    public static JinkanClient getInstance() {
+        if (instance == null) {
+            instance = new JinkanClient();
+        }
+        return instance;
+    }
+
+    public JinkanInterface getMyApi() {
+        return myAPI;
+    }
+
+}
