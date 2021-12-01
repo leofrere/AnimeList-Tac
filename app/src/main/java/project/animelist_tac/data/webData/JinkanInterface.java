5fb328c962029e654aa1ce8002ae446deff0d579
leofrere <leofrere94@gmail.com>
2021-12-01 16:15:32 +0100

    add limit parameters

--- app/src/main/java/project/animelist_tac/data/webData/JinkanInterface.java
+++ app/src/main/java/project/animelist_tac/data/webData/JinkanInterface.java
@@ -7,5 +7,5 @@ import retrofit2.http.Query;
 
 public interface JinkanInterface {
     @GET("?rapidapi-host=jikan1.p.rapidapi.com&rapidapi-key=c09a52467emsh5bc86d47ca19a3bp14d8bcjsn71c384fcf21f")
-    Single<Response> getAnimes(@Query("q") String search);
+    Single<Response> getAnimes(@Query("q") String search, @Query("limit") int numberOfAnime);
 }
